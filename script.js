9515b6a71dfbbf9f7c9e65631272340dfac778af
oludare89 <azizoyekan@hotmail.com>
2022-10-24 21:48:32 +0000

    Created enemy class

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-enemy1 = {
+/* enemy1 = {
     x: 0,
     y: 0,
     width: 200,
     height: 200,
+} */
+
+class Enemy {
+    constuctor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
 }
 
 function animate() {
