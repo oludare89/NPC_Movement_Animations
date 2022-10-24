734578614ae47dc7748950b7433ce4000c50bdc9
oludare89 <azizoyekan@hotmail.com>
2022-10-24 21:52:47 +0000

    added update function for x and y positions in the Enemy class

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-/* enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
-} */
-
 class Enemy {
     constuctor(){
         this.x = 10;
@@ -18,10 +11,16 @@ class Enemy {
         this.width = 100;
         this.height = 100;
     }
+    update(){
+        this.x++;
+        this.y++;
+    }
 }
+const enemy1 = new Enemy();
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.update();
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
     requestAnimationFrame(animate);
 }
