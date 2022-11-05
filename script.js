0f76ae2f047bcc16dab59f624278a075f6c942fe
oludare89 <azizoyekan@hotmail.com>
2022-11-05 10:45:31 +0000

    Generated enemy1 sprites

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 10;
 const enemiesArray = [];
 
+const enemyImage = new Image();
+enemyImage.src = 'assets/images/enemy1.png';
+
 class Enemy {
     constructor(){
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
         this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
     }
     update(){
         this.x += this.speed;
@@ -20,13 +25,12 @@ class Enemy {
     }
     draw(){
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
 for (let i=0; i < numberOfEnemies; i++) {
     enemiesArray.push(new Enemy());
-    console.log(`enemy ${i}`);
-    console.log(enemiesArray);
 }
 
 
