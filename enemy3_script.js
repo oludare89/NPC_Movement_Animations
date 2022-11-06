54ce1999baabce3d7881cbbd5d735daa8f4d7a08
oludare89 <azizoyekan@hotmail.com>
2022-11-06 16:18:41 +0000

    Added enemy3 basic movement pattern

--- enemy3_script.js
+++ enemy3_script.js
@@ -13,8 +13,8 @@ class Enemy {
         this.image = new Image();
         this.image.src = 'assets/images/enemy3.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -22,12 +22,12 @@ class Enemy {
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = Math.random() * 2;
-        this.angleSpeed = Math.random() * 0.2;
+        this.angleSpeed = Math.random() * 2;
         this.curve = Math.random() * 5;
     }
     update(){
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
+        this.x = 100 * Math.sin(this.angle * Math.PI/180) + canvas.width/2 - this.width;
+        // this.y += this.curve * Math.sin(this.angle);
         this.angle += this.angleSpeed;
         if (this.x + this.width < 0){
             this.x = canvas.width;
