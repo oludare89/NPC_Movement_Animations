c864bcdc6b27f0846ecb8626f418e1a480ce87fb
oludare89 <azizoyekan@hotmail.com>
2022-11-06 16:49:42 +0000

    Added basic enemy4 movement

--- enemy4_script.js
+++ enemy4_script.js
@@ -11,7 +11,7 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = 'assets/images/enemy3.png';
+        this.image.src = 'assets/images/enemy4.png';
         this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 218;
         this.spriteHeight = 177;
@@ -19,16 +19,20 @@ class Enemy {
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = Math.random() * 2;
-        this.angleSpeed = Math.random() * 2 + 0.5;
-        // this.curve = Math.random() * 200 + 50;
     }
     update(){
-        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
-        this.angle += this.angleSpeed;
+        if (gameFrame % 30 === 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/30;
+        this.y -= dy/30;
         if (this.x + this.width < 0){
             this.x = canvas.width;
         }
