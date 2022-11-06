94dc401f925c21664cbcd0c73b6d6f5c180c77d5
oludare89 <azizoyekan@hotmail.com>
2022-11-06 08:10:30 +0000

    Amended this.x and this.y so that enemies remain witin the canvas

--- script.js
+++ script.js
@@ -12,26 +12,26 @@ class Enemy {
     constructor(){
         this.image = new Image();
         this.image.src = 'assets/images/enemy1.png';
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         // animate sprites
         if (gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }        
     }
     draw(){
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
